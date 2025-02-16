e4ce366f537b9cacc88aca1fa23df772f22fb027
Johrespi <johrespi@espol.edu.ec>
2025-02-16 02:20:52 -0500

    feature: dark/light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { SkillsComponent } from './components/skills/skills.component';
 import { ContactComponent } from './components/contact/contact.component'; 
 import { AboutComponent } from './components/about/about.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,36 @@ import { AboutComponent } from './components/about/about.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isDarkMode: boolean = true;
   title = 'portfolio';
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'light') {
+        this.isDarkMode = false;
+        this.renderer.removeClass(document.body, 'dark-mode');
+      } else {
+        this.isDarkMode = true;
+        this.renderer.addClass(document.body, 'dark-mode');
+      }
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-mode');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
