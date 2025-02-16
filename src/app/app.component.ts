011699aa00634d7f6367a8fcd54e4f33a64daaa0
Johrespi <johrespi@espol.edu.ec>
2025-02-15 19:41:28 -0500

    adding technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component'; 
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SkillsComponent, ContactComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -8,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  categories = [
+    {
+      title: 'Lenguajes de Programación',
+      techs: [
+        { name: 'Ruby', color:'#CC342D', svg: 'https://cdn.simpleicons.org/ruby'},
+        { name: 'JavaScript', color: '#F7DF1E', svg: 'https://cdn.simpleicons.org/javascript' },
+        { name: 'TypeScript', color: '#3178C6', svg: 'https://cdn.simpleicons.org/typescript' },
+        { name: 'Python', color: '#3776AB', svg: 'https://cdn.simpleicons.org/python' },
+        { name: 'C', color: '#A8B9CC', svg: 'https://cdn.simpleicons.org/c' },
+      ]
+    },
+    {
+      title: 'Frameworks y Librerías',
+      techs: [
+        { name: 'Angular', color: '#DD0031', svg: 'https://cdn.simpleicons.org/angular' },
+        { name: 'Ruby on Rails', color: '#D30001', svg: 'https://cdn.simpleicons.org/rubyonrails' },
+        { name: 'React', color: '#61DAFB', svg: 'https://cdn.simpleicons.org/react' }
+      ]
+    },
+    {
+      title: 'Bases de Datos',
+      techs: [
+        { name: 'MySQL', color: '#4479A1', svg: 'https://cdn.simpleicons.org/mysql' },
+        { name: 'PostgreSQL', color: '#336791', svg: 'https://cdn.simpleicons.org/postgresql' },
+        { name: 'SQLite', color: '#003B57', svg: 'https://cdn.simpleicons.org/sqlite' }
+      ]
+    },
+    {
+      title: 'Herramientas y DevOps',
+      techs: [
+        { name: 'Git', color: '#F05032', svg: 'https://cdn.simpleicons.org/git' },
+        { name: 'GitHub', color: '#181717', svg: 'https://cdn.simpleicons.org/github' }
+      ]
+    }
+  ]
 }
